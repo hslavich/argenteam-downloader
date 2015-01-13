12af39d5d601fe2d72df4a19068917f16bcb62fd
hslavich <hernan.slavich@gmail.com>
2015-01-13 18:04:22 -0300

    Improve output

--- argenteamsub.py
+++ argenteamsub.py
@@ -8,6 +8,9 @@ api_url = "http://argenteam.net/api/v1/"
 api_search = api_url + "search"
 api_episode = api_url + "episode"
 
+def release_to_string(release):
+    return "%s %s %s %s" % (release['source'], release['codec'], release['tags'], release['team'])
+
 def match_version(release, version):
     if version:
         return release['tags'] in version and release['team'] in version
@@ -18,14 +21,24 @@ def download_sub(id, version):
     content = json.loads(response)
 
     for release in content['releases']:
+        release_name = release_to_string(release)
         if match_version(release, version):
-            sub = release['subtitles'][0]['uri']
-            (filename, _) = urllib.urlretrieve(sub)
-            zipfile.ZipFile(filename).extractall()
-            return sub
+            print "Version match: " + release_name
+            if release['subtitles']:
+                sub = release['subtitles'][0]['uri']
+                (filename, _) = urllib.urlretrieve(sub)
+                zipfile.ZipFile(filename).extractall()
+                return sub
+            else:
+                print "No subtitles found for " + release_name
+        else:
+            print "Ignoring version: " + release_name
+
+    return None
 
 def search_sub(tvshow, season, episode, version=""):
     search = "%s s%#02de%#02d" % (tvshow, season, episode)
+    print "Searching subtitles for: %s. Version: %s" % (search, version)
     response = urllib.urlopen(api_search + "?q=" + urllib.quote_plus(search)).read()
     content = json.loads(response)
     for result in content['results']:
